535716ebe1b3f0f84d3a45fc11df16543e52b2b6
hscho1226@gmail.com <hscho1226@gmail.com>
2015-01-25 23:40:10 +0900

    images

--- app/src/main/java/com/example/chocho/lifegraph/MainActivity.java
+++ app/src/main/java/com/example/chocho/lifegraph/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton addButton = (ImageButton)findViewById(R.id.addButton);
+        addButton.setBackground(null);
     }
 
 
