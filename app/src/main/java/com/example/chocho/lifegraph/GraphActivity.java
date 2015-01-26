c7310bb9592f1675744dadea82ae259e17d1ad1f
taehyun <taehyun9@kaist.ac.kr>
2015-01-26 21:28:25 +0900

    Merge branch 'master' of C:\Users\Hwang\Desktop\O_O\Study\Android\LifeGraph with conflicts.

--- app/src/main/java/com/example/chocho/lifegraph/GraphActivity.java
+++ app/src/main/java/com/example/chocho/lifegraph/GraphActivity.java
@@ -1,9 +1,17 @@
 package com.example.chocho.lifegraph;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 /**
  * Created by chocho on 2015-01-25.
  */
 public class GraphActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_graph);
+    }
 }
--- app/src/main/java/com/example/chocho/lifegraph/ListActivity.java
+++ app/src/main/java/com/example/chocho/lifegraph/ListActivity.java
@@ -1,9 +1,18 @@
 package com.example.chocho.lifegraph;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by chocho on 2015-01-25.
  */
 public class ListActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
 }
+
+
+//e6e6e6
