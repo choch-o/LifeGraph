5b8c5da79ab9f031a56f4d05b83be2db75fffd01
hscho1226@gmail.com <hscho1226@gmail.com>
2015-01-27 19:20:34 +0900

    Main, Event

--- app/src/main/java/com/example/chocho/lifegraph/EventActivity.java
+++ app/src/main/java/com/example/chocho/lifegraph/EventActivity.java
@@ -1,9 +1,99 @@
 package com.example.chocho.lifegraph;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by chocho on 2015-01-25.
  */
 public class EventActivity extends Activity {
+    ArrayList<String> ageList = new ArrayList<String>();
+    ArrayList<String> scoreList = new ArrayList<String>();
+    ArrayList<String> categoryList = new ArrayList<String>();
+
+    Button event_done;
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+
+        Spinner age_spinner = (Spinner) findViewById(R.id.age_spinner);
+        Spinner score_spinner = (Spinner) findViewById(R.id.score_spinner);
+        Spinner category_spinner = (Spinner) findViewById(R.id.category_spinner);
+
+        for (int i = 0; i<=100; i++) {
+            ageList.add(Integer.toString(i));
+        }
+
+        for (int i = 10; i>=0; i--) {
+            scoreList.add(Integer.toString(i));
+        }
+        for (int i = 1; i<=10; i++) {
+            scoreList.add("-"+Integer.toString(i));
+        }
+
+        categoryList.add("학업");
+        categoryList.add("여행");
+        categoryList.add("만남");
+
+        //Create the ArrayAdapter
+        ArrayAdapter<String> ageAdapter = new ArrayAdapter<String>(EventActivity.this, android.R.layout.simple_spinner_item, ageList);
+        ArrayAdapter<String> scoreAdapter = new ArrayAdapter<String>(EventActivity.this, android.R.layout.simple_spinner_item, scoreList);
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String>(EventActivity.this, android.R.layout.simple_spinner_item, categoryList);
+
+        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        scoreAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Set the Adapter
+        age_spinner.setAdapter(ageAdapter);
+        score_spinner.setAdapter(scoreAdapter);
+        category_spinner.setAdapter(categoryAdapter);
+
+        //Set the ClickListener for Spinner
+        age_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(EventActivity.this, "You Selected : " + ageList.get(position)+" Level ",Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        score_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(EventActivity.this, "You Selected : " + ageList.get(position)+" Level ",Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        category_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(EventActivity.this, "You Selected : " + ageList.get(position)+" Level ",Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        event_done = (Button) findViewById(R.id.event_done);
+        event_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/chocho/lifegraph/MainActivity.java
+++ app/src/main/java/com/example/chocho/lifegraph/MainActivity.java
@@ -1,24 +1,55 @@
 package com.example.chocho.lifegraph;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
+    final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageButton addButton = (ImageButton)findViewById(R.id.addButton);
         addButton.setBackground(null);
+        addButton.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addButton:
+                LayoutInflater li = LayoutInflater.from(context);
+                View dialogView = li.inflate(R.layout.main_dialog, null);
 
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
 
+                alertDialogBuilder.setView(dialogView);
+
+                alertDialogBuilder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                       public void onClick(DialogInterface dialog, int id) {
+                           Intent intent = new Intent(context, GraphActivity.class);
+                           startActivity(intent);
+                       }
+                });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                alertDialog.show();
+                break;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
